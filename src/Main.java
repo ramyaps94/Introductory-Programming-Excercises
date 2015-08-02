d2119e280a490ea56b1c5f38bf3143a01dfd67c8
ramyaps94 <ramya.sudarshan1994@gmail.com>
2015-08-02 15:46:01 +0530

    |006| added functionality to draw a right triangle of asterisk in ascending order

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         AnAsterisk asterisk = new AnAsterisk();
         HorizontalLine nAsterisk = new HorizontalLine();
         VerticalLine asteriskPerLine = new VerticalLine();
+        RightTriangle asteriskTriangle = new RightTriangle();
         separator.drawSeparation("prints an asterisk");
         asterisk.printAnAsterisk();
         int number = 8;
@@ -11,5 +12,7 @@ public class Main {
         nAsterisk.printNAsterisk(number);
         separator.drawSeparation("prints vertical line of asterisk");
         asteriskPerLine.printAnAsteriskPerLine(number);
+        separator.drawSeparation("prints a right triangle with asterisk in each line in ascending order");
+        asteriskTriangle.drawRightTriangle(number);
     }
 }
---
+++ src/RightTriangle.java
@@ -0,0 +1,10 @@
+public class RightTriangle {
+    public void drawRightTriangle(int number) {
+        for (int i = 0; i < number; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
