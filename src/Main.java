9985ffc67e5fa53373202d073152171828e2599d
ramyaps94 <ramya.sudarshan1994@gmail.com>
2015-08-02 15:08:08 +0530

    |003| prints an asterisk on each line on the console

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ public class Main {
     public static void main(String args[]) {
         PrintAnAsterisk asterisk = new PrintAnAsterisk();
         PrintNAsterisk nAsterisk = new PrintNAsterisk();
+        PrintAnAsteriskInEachLine asteriskPerLine = new PrintAnAsteriskInEachLine();
         asterisk.printAnAsterisk();
         int number = 8;
         nAsterisk.printNAsterisk(number);
+        asteriskPerLine.printAnAsteriskPerLine(number);
     }
 }
---
+++ src/PrintAnAsteriskInEachLine.java
@@ -0,0 +1,9 @@
+public class PrintAnAsteriskInEachLine {
+    public void printAnAsteriskPerLine(int number) {
+    int i= 0;
+        while (i < number){
+            System.out.println("*");
+            i++;
+        }
+    }
+}
